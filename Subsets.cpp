55d1265585bfb09d9b5b698f1cc44cde9a679bae
Blockman66 <48730937+Blockman66@users.noreply.github.com>
2019-03-19 15:50:45 -0400

    Update Subsets.cpp

--- Subsets.cpp
+++ Subsets.cpp
@@ -14,3 +14,53 @@ public:
     return subs;
     }
 };
+
+--------------------------------------------------------------
+    
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        
+    int subindex = 0;
+    vector<int> subset;
+    vector<vector<int>> result;
+        
+    vector<int> copy(nums);
+        
+    Sort(copy);
+    dpsHandle(copy, subindex, subset, result);
+        
+    return result;
+    }
+    
+private:
+    void Sort(vector<int>& vec){
+        
+        for(int i = 0; i < vec.size(); i++){
+            
+            for(int j = 0; j < vec.size() - 1 - i; j++){
+                
+                if(vec[j] < vec[j+1]){
+                    int temp = vec[j+1];
+                    vec[j+1] = vec[j];
+                    vec[j] = temp;
+                }
+        }
+    }
+    }
+        
+    void dpsHandle(vector<int>& copy,
+                   int subindex,
+                   vector<int>& subset,
+                   vector<vector<int>>& result){
+        
+        result.push_back(subset);
+            for(int j = subindex; j < copy.size(); j++){
+                subset.push_back(copy[j]);
+                dpsHandle(copy, j + 1, subset, result);
+                subset.pop_back();
+            }
+        
+    }
+        
+};
